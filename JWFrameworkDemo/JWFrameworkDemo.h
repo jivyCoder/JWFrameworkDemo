b5f525ce78f11ade7412dab2fc5fa81a8a80543d
jiawei <513590934@qq.com>
2018-12-13 16:47:59 +0800

    Initial Commit

---
+++ JWFrameworkDemo/JWFrameworkDemo.h
@@ -0,0 +1,19 @@
+//
+//  JWFrameworkDemo.h
+//  JWFrameworkDemo
+//
+//  Created by Ealliance on 2018/12/13.
+//  Copyright © 2018 jw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JWFrameworkDemo.
+FOUNDATION_EXPORT double JWFrameworkDemoVersionNumber;
+
+//! Project version string for JWFrameworkDemo.
+FOUNDATION_EXPORT const unsigned char JWFrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWFrameworkDemo/PublicHeader.h>
+
+
